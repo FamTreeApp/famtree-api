e1ea3786c82f27481498f25bb98c13c364389811
gilangcy <gilangcaturyudistira@gmail.com>
2022-12-21 22:16:52 +0700

    add google auth

--- config/auth.go
+++ config/auth.go
@@ -10,7 +10,7 @@ import (
 func SetupAuth() {
 	key := "gilangcy-key" // Replace with your SESSION_SECRET or similar
 	maxAge := 86400 * 30  // 30 days
-	isProd := false       // Set to true when serving over https
+	isProd := true        // Set to true when serving over https
 
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
@@ -20,7 +20,14 @@ func SetupAuth() {
 
 	gothic.Store = store
 
-	goth.UseProviders(
-		google.New("297925112549-flbk68gc74acqr3untoluhikiqtrenst.apps.googleusercontent.com", "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", "http://localhost:3000/auth/google/callback", "email", "profile"),
-	)
+	if isProd {
+		goth.UseProviders(
+			google.New("297925112549-flbk68gc74acqr3untoluhikiqtrenst.apps.googleusercontent.com", "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", "https://my-open-source-project-372305.uc.r.appspot.com/auth/google/callback", "email", "profile"),
+		)
+	} else {
+		goth.UseProviders(
+			google.New("297925112549-flbk68gc74acqr3untoluhikiqtrenst.apps.googleusercontent.com", "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", "http://localhost:8080/auth/google/callback", "email", "profile"),
+		)
+	}
+
 }
